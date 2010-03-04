cf73c6caf6981a987c144962a89663de7d69e08a
John David Eriksen <john.eriksen@yahoo.com>
2010-03-03 21:56:56 -0500

    Addition of images and partials. Partial completion of php -> ror conversion process. TODO: fix remaining links and images. Add in partials. Implement "body" and "eyes" gallery functionality.

--- velvetseahorse/app/controllers/public_controller.rb
+++ velvetseahorse/app/controllers/public_controller.rb
@@ -1,2 +1,21 @@
 class PublicController < ApplicationController
+    layout 'public'
+
+    def index
+    end
+
+    def news
+    end
+
+    def body
+    end
+    
+    def eyes
+    end
+
+    def about
+    end
+    
+    def curiosity
+    end
 end
